4aa8f997c2fa8686c472d5ceaa849b03ab340e57
josepo <jlpolo@gmail.com>
2021-06-04 18:52:54 +0200

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Item from './components/Item';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Expenses Manager <br />
-          <Item />
-          <Item />
-          <Item />
-        </p>
-      </header>
+      <Item />
+      <Item />
+      <Item />
     </div>
   );
 }
